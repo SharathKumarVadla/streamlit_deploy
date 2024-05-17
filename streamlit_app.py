8662802b0d8d4f9723275183856868910332c0a3
Sharathvadla123 <115218705+Sharathvadla123@users.noreply.github.com>
2024-05-17 14:08:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,18 @@ from keras.models import load_model
 import pickle
 import numpy as np
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 with open('labels.pkl', 'rb') as f:
         labels = pickle.load(f)
